e2c8592771575cc8048d30e29c4b30ef3e90023a
dudaodong <dudaodong@gmail.com>
2022-08-13 14:46:07 +0800

    init project

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div className="grid place-content-center text-center text-lg">
+      <div className="flex mx-auto items-center gap-2">
+        React项目工程模板: Vite + React + Ant Design + Tailwind CSS + ESLint +
+        Prettier + TypeScript.
+      </div>
+    </div>
+  );
+};
+
+export default About;
